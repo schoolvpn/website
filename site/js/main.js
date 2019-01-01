6771c03223a5af395b8af4efb89e93035970cd59
iNcizzle <incizzle@gmail.com>
2019-01-01 18:51:47 -0500

    Removed lifeLike typing.

--- site/js/main.js
+++ site/js/main.js
@@ -16,6 +16,7 @@
 new TypeIt('#maintitle', {
     strings: ["Schoolvpn.ca!", "The Fastest & Most Secure VPN", "Schoolvpn.ca!"],
     speed: 100,
+    lifeLike: false,
     breakLines: false,
     autoStart: false
 });
