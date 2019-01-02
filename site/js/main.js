34e31e970cb60daa47203f606c4535a66ebe1cdb
iNcizzle <incizzle@gmail.com>
2019-01-01 20:31:51 -0500

    Better Version Number

--- site/js/main.js
+++ site/js/main.js
@@ -13,8 +13,20 @@
 
 })(jQuery);
 
-new TypeIt('#maintitle', {
-    strings: ["Schoolvpn.ca!", "The Fastest & Most Secure VPN", "Schoolvpn.ca!"],
+tippy.setDefaults({
+    delay: 100,
+    allowHTML: true,
+    arrow: true,
+    theme: "light",
+    arrowType: 'round',
+    size: 'small',
+    duration: 100,
+    placement: 'bottom',
+    animation: 'scale'
+  })
+
+new TypeIt('#maintitle',{
+    strings: ["Schoolvpn.ca", "The Fastest & Most Secure VPN", "Schoolvpn.ca"],
     speed: 100,
     lifeLike: false,
     breakLines: false,
