f4f2588921295033fe60c1a0b0a1045a5bdada63
iNcizzle <incizzle@gmail.com>
2019-01-04 03:52:10 -0500

    Big Update :)

--- site/js/main.js
+++ site/js/main.js
@@ -32,3 +32,15 @@ new TypeIt('#maintitle',{
     breakLines: false,
     autoStart: false
 });
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
